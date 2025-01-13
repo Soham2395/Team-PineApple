e146ff2b8b7d625dd8f3ff2d4b138d7d66393522
Soham2395 <soham2395.be22@chitkara.edu.in>
2025-01-13 09:52:37 +0530

    Initial Commit

---
+++ iOS PulseFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS PulseFit
+//
+//  Created by Soham Chakraborty on 13/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS PulseFitTests/iOS_PulseFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_PulseFitTests.swift
+//  iOS PulseFitTests
+//
+//  Created by Soham Chakraborty on 13/01/25.
+//
+
+import Testing
+@testable import iOS_PulseFit
+
+struct iOS_PulseFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
